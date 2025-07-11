3ae99b40d509ae6a2d8fda5649acd13d978fe54b
KadoProG <sho.kadoguchi@sun-asterisk.com>
2025-07-11 12:00:23 +0900

    feat: ハッシュ関数追加

---
+++ src/utils/urlBase64ToUint8Array.ts
@@ -0,0 +1,7 @@
+// Base64形式 → Uint8Array に変換
+export default function urlBase64ToUint8Array(base64String: string) {
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
+  const raw = window.atob(base64);
+  return Uint8Array.from([...raw].map((c) => c.charCodeAt(0)));
+}
